ba18a08d1b29943b6b6c3b52b85a2f211de5a130
KaiwenTM <add-me@live.com.sg>
2019-11-27 14:25:12 +0800

    chatbot_is_reset

--- ini.h
+++ ini.h
@@ -14,8 +14,8 @@ typedef struct ini_t ini_t;
 
 ini_t*      ini_load(const char *filename);
 void        ini_free(ini_t *ini);
-const char* ini_get(ini_t *ini, const char *section, const char *key, char *string);
-void        ini_write(ini_t *ini, const char *section, const char *key, const char *string);
-int         ini_sget(ini_t *ini, const char *section, const char *key, const char *scanfmt, void *dst);
+int ini_get(ini_t *ini, const char *section, const char *key, char *string);
+int        ini_write(ini_t *ini, const char *section, const char *key, const char *string);
+int        ini_sget(ini_t *ini, const char *section, const char *key, const char *scanfmt, void *dst);
 
 #endif
